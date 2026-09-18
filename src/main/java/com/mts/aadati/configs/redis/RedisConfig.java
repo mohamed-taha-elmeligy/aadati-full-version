b541b4890729fff0b609932b0a21a80c84440587
mohamed742590taha-wq <mohamed742590taha@gmail.com>
2026-09-18 22:50:42 +0300

    feat: Configure RedisTemplate with JSON value serialization

---
+++ src/main/java/com/mts/aadati/configs/redis/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.mts.aadati.configs.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory);
+
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
